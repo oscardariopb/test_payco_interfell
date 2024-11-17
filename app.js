ab56ddf5f2acc810ec3db8105ec517ec8360b6c4
oscardariopb <cronos1610@hotmail.com>
2024-11-17 18:47:06 -0500

    Get Wallet Complete

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const clientSchema = new mongoose.Schema({
     documento: { type: String, required: true },
     email: { type: String, required: true },
     celular: { type: String, required: true },
-  });
+});
 
 const ClientModel = mongoose.model('Client', clientSchema);
 
@@ -24,12 +24,10 @@ const walletSchema = new mongoose.Schema({
     documentocliente: {type: String, required: true},
     celular: {type: String, required: true},
     saldo: { type: Number, required: true },
-  });
+});
 
 const WalletModel = mongoose.model('Wallet', walletSchema);
 
-
-// Iniciamos el servidor Express
 const port = 3000;
 
 function createResponse(success, cod_error, message_error, data) {
@@ -39,33 +37,24 @@ function createResponse(success, cod_error, message_error, data) {
       message_error,
       data
     };
-  }
+}
 
-// Simulamos una base de datos en memoria
-let usuarios = [
-  { id: 1, nombre: 'Juan', edad: 30 },
-  { id: 2, nombre: 'Ana', edad: 25 },
-];
 
-// Definir los métodos de SOAP para el CRUD
 const service = {
-  UsuarioService: {
-    UsuarioServicePort: {
-      // Obtener todos los usuarios (simulando un GET)
-      getUsuarios: function () {
-        return { usuarios: usuarios };
-      },
-      // Obtener un usuario por ID (simulando un GET con parámetro)
-      getUsuario: function (args) {
-        console.log('get usuario', args)
-        const usuario = usuarios.find(u => u.id === parseInt(args.id));
-        if (usuario) {
-          return { usuario: usuario };
-        } else {
-          return { error: 'Usuario no encontrado' };
+  PaycoService: {
+    PaycoServicePort: {
+      getWallet: async function (args) {
+        try {
+            const wallet = await WalletModel.findOne({documentocliente: args.documento, celular: args.celular});
+            if (wallet) {
+              return createResponse(true, '00', '', JSON.stringify(wallet));
+            } else {
+              return createResponse(true, '00', 'No wallet found', {});
+            }
+        } catch (err) {
+          return createResponse(false, '500', 'Cannot search wallet: ' + err.message, {});
         }
       },
-      // Crear un nuevo usuario (simulando un POST)
       createClient: async function (args) {
         try {
             const newClient = new ClientModel({
@@ -86,25 +75,13 @@ const service = {
           } catch (err) {
             return createResponse(false, '500', 'Cannot update wallet: ' + err.message, {});
           }
-      },
-      // Actualizar un usuario existente (simulando un PUT)
-      actualizarUsuario: function (args) {
-        const usuario = usuarios.find(u => u.id === parseInt(args.id));
-        if (usuario) {
-          usuario.nombre = args.nombre;
-          usuario.edad = parseInt(args.edad);
-          return { usuario: usuario };
-        } else {
-          return { error: 'Usuario no encontrado' };
-        }
-      },
+      }
     }
   }
 };
 
 // Configuración del servidor SOAP
 const server = http.createServer((req, res) => {
-    //console.log('entree '. process);
   res.end('Servidor SOAP corriendo');
 });
 
--- wsdl.js
+++ wsdl.js
@@ -1,18 +1,17 @@
 const wsdl = `<?xml version="1.0" encoding="UTF-8"?>
 <definitions xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
-             xmlns:web="http://www.example.com/usuarios"
-             targetNamespace="http://www.example.com/usuarios">
-  <message name="getUsuariosRequest"/>
-  <message name="getUsuariosResponse">
-    <part name="usuarios" type="xsd:string"/>
-  </message>
+             xmlns:web="http://www.example.com/payco"
+             targetNamespace="http://www.example.com/payco">
 
-  <message name="getUsuarioRequest">
-    <part name="id" type="xsd:int"/>
+  <message name="getWalletRequest">
+    <part name="documento" type="xsd:string"/>
+    <part name="celular" type="xsd:string"/>
   </message>
-  <message name="getUsuarioResponse">
-    <part name="usuario" type="xsd:string"/>
-    <part name="error" type="xsd:string"/>
+  <message name="getWalletResponse">
+    <part name="success" type="xsd:boolean"/>
+    <part name="cod_error" type="xsd:string"/>
+    <part name="message_error" type="xsd:string"/>
+    <part name="data" type="xsd:string"/>
   </message>
 
   <message name="createClientRequest">
@@ -40,24 +39,10 @@ const wsdl = `<?xml version="1.0" encoding="UTF-8"?>
     <part name="data" type="xsd:string"/>
   </message>
 
-  <message name="actualizarUsuarioRequest">
-    <part name="id" type="xsd:int"/>
-    <part name="nombre" type="xsd:string"/>
-    <part name="edad" type="xsd:int"/>
-  </message>
-  <message name="actualizarUsuarioResponse">
-    <part name="usuario" type="xsd:string"/>
-    <part name="error" type="xsd:string"/>
-  </message>
-
-  <portType name="UsuarioServicePortType">
-    <operation name="getUsuarios">
-      <input message="web:getUsuariosRequest"/>
-      <output message="web:getUsuariosResponse"/>
-    </operation>
-    <operation name="getUsuario">
-      <input message="web:getUsuarioRequest"/>
-      <output message="web:getUsuarioResponse"/>
+  <portType name="PaycoServicePortType">
+    <operation name="getWallet">
+      <input message="web:getWalletRequest"/>
+      <output message="web:getWalletResponse"/>
     </operation>
     <operation name="createClient">
       <input message="web:createClientRequest"/>
@@ -67,63 +52,41 @@ const wsdl = `<?xml version="1.0" encoding="UTF-8"?>
       <input message="web:updateWalletClientRequest"/>
       <output message="web:updateWalletClientResponse"/>
     </operation>
-    <operation name="actualizarUsuario">
-      <input message="web:actualizarUsuarioRequest"/>
-      <output message="web:actualizarUsuarioResponse"/>
-    </operation>
   </portType>
 
-  <binding name="UsuarioServiceBinding" type="web:UsuarioServicePortType">
+  <binding name="PaycoServiceBinding" type="web:PaycoServicePortType">
     <soap:binding style="rpc" transport="http://schemas.xmlsoap.org/soap/http"/>
-    <operation name="getUsuarios">
-      <soap:operation soapAction="getUsuarios"/>
+    <operation name="getWallet">
+      <soap:operation soapAction="getWallet"/>
       <input>
-        <soap:body use="encoded" namespace="http://www.example.com/usuarios"/>
+        <soap:body use="encoded" namespace="http://www.example.com/payco"/>
       </input>
       <output>
-        <soap:body use="encoded" namespace="http://www.example.com/usuarios"/>
-      </output>
-    </operation>
-    <operation name="getUsuario">
-      <soap:operation soapAction="getUsuario"/>
-      <input>
-        <soap:body use="encoded" namespace="http://www.example.com/usuarios"/>
-      </input>
-      <output>
-        <soap:body use="encoded" namespace="http://www.example.com/usuarios"/>
+        <soap:body use="encoded" namespace="http://www.example.com/payco"/>
       </output>
     </operation>
     <operation name="createClient">
       <soap:operation soapAction="createClient"/>
       <input>
-        <soap:body use="encoded" namespace="http://www.example.com/usuarios"/>
+        <soap:body use="encoded" namespace="http://www.example.com/payco"/>
       </input>
       <output>
-        <soap:body use="encoded" namespace="http://www.example.com/usuarios"/>
+        <soap:body use="encoded" namespace="http://www.example.com/payco"/>
       </output>
     </operation>
     <operation name="updateWalletClient">
       <soap:operation soapAction="updateWalletClient"/>
       <input>
-        <soap:body use="encoded" namespace="http://www.example.com/usuarios"/>
+        <soap:body use="encoded" namespace="http://www.example.com/payco"/>
       </input>
       <output>
-        <soap:body use="encoded" namespace="http://www.example.com/usuarios"/>
-      </output>
-    </operation>
-    <operation name="actualizarUsuario">
-      <soap:operation soapAction="actualizarUsuario"/>
-      <input>
-        <soap:body use="encoded" namespace="http://www.example.com/usuarios"/>
-      </input>
-      <output>
-        <soap:body use="encoded" namespace="http://www.example.com/usuarios"/>
+        <soap:body use="encoded" namespace="http://www.example.com/payco"/>
       </output>
     </operation>
   </binding>
 
-  <service name="UsuarioService">
-    <port name="UsuarioServicePort" binding="web:UsuarioServiceBinding">
+  <service name="PaycoService">
+    <port name="PaycoServicePort" binding="web:PaycoServiceBinding">
       <soap:address location="http://localhost:3000/soap"/>
     </port>
   </service>
